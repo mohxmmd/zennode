5084f10366c8068b2ae6e76113dc4b9776b6b956
Mohammed <140478331+mohxmmd@users.noreply.github.com>
2024-01-17 02:45:14 +0530

    Add files via upload

---
+++ Zennode_task1_javascript.js
@@ -0,0 +1,108 @@
+console.log('catalogue');
+console.log('Product A : $20');
+console.log('Product B : $40');
+console.log('Product C : $50');
+
+// getting quantity and whether gift wrapping is required or not for each product
+let quantityA = parseInt(prompt('Enter the required quantity for Product A:'));
+let giftwrapA = parseInt(prompt('Gift wrap required or not (1/0): '));
+let quantityB = parseInt(prompt('Enter the required quantity for Product B: '));
+let giftwrapB = parseInt(prompt('Gift wrap required or not (1/0): '));
+let quantityC = parseInt(prompt('Enter the required quantity for Product C: '));
+let giftwrapC = parseInt(prompt('Gift wrap required or not (1/0): '));
+
+let totalAmountA = quantityA*20;
+let totalAmountB = quantityB*40;
+let totalAmountC = quantityC*50;
+let totalAmount = totalAmountA + totalAmountB + totalAmountC;
+
+// Checking the total quantity in cart to apply flat_10_discount
+let flat_10_discount = 0;
+if (totalAmount > 200){
+    flat_10_discount = 10;
+}
+
+// checking each product quantity to apply bulk_5_discount
+let bulk_5_discountA = 0;
+let bulk_5_discountB = 0;
+let bulk_5_discountC = 0;
+
+if (quantityA > 10){
+    bulk_5_discountA = totalAmountA * 0.05;
+}
+if (quantityB > 10){
+    bulk_5_discountB = totalAmountB * 0.05;
+}
+if (quantityC > 10){
+    bulk_5_discountC = totalAmountC * 0.05;
+}
+let bulk_5_discount = bulk_5_discountA + bulk_5_discountB + bulk_5_discountC;
+
+//checking total cart quantity to apply bulk_10_discount
+let bulk_10_discount = 0
+if ((quantityA + quantityB + quantityC) > 20){
+    bulk_10_discount = totalAmount*0.1;
+}
+
+//checking and applying tiered_50_discount
+let tiered_50_discountA = 0;
+let tiered_50_discountB = 0;
+let tiered_50_discountC = 0;;
+let totalQuantity = quantityA + quantityB + quantityC;
+if (totalQuantity > 30 && quantityA > 15){
+    let discountableA = quantityA - 15;
+    tiered_50_discountA = discountableA*0.5;
+}
+if (totalQuantity > 30 && quantityB > 15){
+    let discountableB = quantityB - 15;
+    tiered_50_discountB= discountableB*0.5;
+}
+if (totalQuantity > 30 && quantityC > 15){
+    let discountableC = quantityC - 15;
+    tiered_50_discountC = discountableC * 0.5;
+}
+let tiered_50_discount = tiered_50_discountA + tiered_50_discountB + tiered_50_discountC;
+
+//Selecting the most profitable discount for customer
+let discount = Math.max(flat_10_discount, bulk_5_discount, bulk_10_discount, tiered_50_discount);
+if (discount == 0){
+    var discountName = 'No Discounts Available';
+}
+else if (discount == flat_10_discount){
+    var discountName = 'flat_10_discount';
+}
+else if (discount == bulk_5_discount){
+    var discountName = 'bulk_5_discount';
+}
+else if (discount == bulk_10_discount){
+    var discountName = 'bulk_10_discount';
+}
+else if (discount == tiered_50_discount){
+    var discountName = 'tiered_50_discount';
+}
+else{
+    var discountName = 'No Discounts Available';
+}
+// calculating gift wrap charges
+let giftwrap = 0
+if (giftwrapA == 1){
+    giftwrap = giftwrap+quantityA;
+}
+if (giftwrapB == 1){
+    giftwrap = giftwrap+quantityB;
+}
+if (giftwrapC == 1){
+    giftwrap = giftwrap+quantityC;;
+}
+
+// calculating shipping fee
+let shippingFee = Math.floor(totalQuantity / 10) * 5;
+
+// printing output
+console.log(`Product: Product A\nQuantity: ${quantityA}\nTotal amount of Product A: $${totalAmountA}`);
+console.log(`Product: Product B\nQuantity: ${quantityB}\nTotal amount of Product A: $${totalAmountB}`);
+console.log(`Product: Product c\nQuantity: ${quantityC}\nTotal amount of Product A: $${totalAmountC}`);
+console.log(`Subtotal: ${totalAmount}`); //Subtotal
+console.log(`Discount applied :${discountName} \nDiscount amount: ${discount}`);  // Discount details
+console.log(`Shipping fee:${shippingFee} \nGiftwrap charge:${giftwrap}`);  // Shipping and giftwrap charges
+console.log(`Total amount: ${(totalAmount+shippingFee+giftwrap)-discount}`);  // Total
---
+++ Zennode_task1_python.py
@@ -0,0 +1,96 @@
+print('----------------------------------------------------------------------------')
+print('catalogue')
+print('----------------------------------------------------------------------------')
+print('Product A : $20')
+print('Product B : $40')
+print('Product C : $50')
+print('----------------------------------------------------------------------------')
+
+# getting quantity and whether gift wrapping is required or not for each product
+quantity_a = int(input('Enter the required quantity for Product A: '))
+giftwrap_a = int(input('Gift wrap required or not (1/0): '))
+quantity_b = int(input('Enter the required quantity for Product B: '))
+giftwrap_b = int(input('Gift wrap required or not (1/0): '))
+quantity_c = int(input('Enter the required quantity for Product C: '))
+giftwrap_c = int(input('Gift wrap required or not (1/0): '))
+
+total_amount_a = quantity_a*20
+total_amount_b = quantity_b*40
+total_amount_c = quantity_c*50
+total_amount = total_amount_a+total_amount_b+total_amount_c
+
+# Checking the total quantity in cart to apply flat_10_discount
+flat_10_discount = 0
+if total_amount > 200:
+    flat_10_discount = 10
+
+# checking each product quantity to apply bulk_5_discount
+bulk_5_discount_a, bulk_5_discount_b, bulk_5_discount_c = 0, 0, 0
+if quantity_a > 10:
+    bulk_5_discount_a = total_amount_a * 0.05
+if quantity_b > 10:
+    bulk_5_discount_b = total_amount_b * 0.05
+if quantity_c > 10:
+    bulk_5_discount_c = total_amount_c * 0.05
+bulk_5_discount = bulk_5_discount_a + bulk_5_discount_b + bulk_5_discount_c
+
+# checking total cart quantity to apply bulk_10_discount
+bulk_10_discount = 0
+if (quantity_a + quantity_b + quantity_c) > 20:
+    bulk_10_discount = total_amount*0.1
+
+# checking and applying tiered_50_discount
+tiered_50_discount_a,tiered_50_discount_b,tiered_50_discount_c = 0, 0, 0
+total_quantity = quantity_a + quantity_b + quantity_c
+if total_quantity > 30 and quantity_a > 15:
+    discountable_a = quantity_a-15
+    tiered_50_discount_a = discountable_a*0.5
+
+if total_quantity > 30 and quantity_b > 15:
+    discountable_b = quantity_b-15
+    tiered_50_discount_b = discountable_b*0.5
+
+if total_quantity > 30 and quantity_c > 15:
+    discountable_c = quantity_c-15
+    tiered_50_discount_c = discountable_c*0.5
+tiered_50_discount = tiered_50_discount_a+tiered_50_discount_b+tiered_50_discount_c
+
+# Selecting the most profitable discount for customer
+discount = max(flat_10_discount, bulk_5_discount, bulk_10_discount, tiered_50_discount)
+if discount == 0:
+    discount_name = 'No Discounts Available'
+elif discount == flat_10_discount:
+    discount_name = 'flat_10_discount'
+elif discount == bulk_5_discount:
+    discount_name = 'bulk_5_discount'
+elif discount == bulk_10_discount:
+    discount_name = 'bulk_10_discount'
+elif discount == tiered_50_discount:
+    discount_name = 'tiered_50_discount'
+else:
+    discount_name = 'No Discounts Available'
+
+# calculating gift wrap charges
+giftwrap = 0
+if giftwrap_a == 1:
+    giftwrap = giftwrap+quantity_a
+if giftwrap_b == 1:
+    giftwrap = giftwrap+quantity_b
+if giftwrap_c == 1:
+    giftwrap = giftwrap+quantity_c
+
+# calculating shipping fee
+shipping_fee = (total_quantity//10)*5
+
+# printing output
+print('----------------------------------------------------------------------------')
+print(f'Product:Product A \nQuantity:{quantity_a}\nTotal amount of Product A:${total_amount_a}')
+print('----------------------------------------------------------------------------')
+print(f'Product:Product B \nQuantity:{quantity_b}\nTotal amount of Product A:${total_amount_b}')
+print('----------------------------------------------------------------------------')
+print(f'Product:Product C \nQuantity:{quantity_c}\nTotal amount of Product A:${total_amount_c}')
+print('----------------------------------------------------------------------------')
+print(f'Subtotal: ${total_amount}')  # Subtotal
+print(f'Discount applied :{discount_name} \nDiscount amount: ${discount}')  # Discount details
+print(f'Shipping fee:${shipping_fee} \nGiftwrap charge:${giftwrap}')  # Shipping and giftwrap charges
+print(f'Total amount: ${(total_amount+shipping_fee+giftwrap)-discount}')  # Total
